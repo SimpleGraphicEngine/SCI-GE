bbec0beb5b981f2ab19f0f7cd386e65e1ae1f874
SimpleGameEngine <asdfasdfasdfasdf@asdf.pl>
2019-01-01 19:00:04 +0100

    Render first triangle.

--- source/sci/Renderer.cpp
+++ source/sci/Renderer.cpp
@@ -19,18 +19,25 @@ Renderer::~Renderer()
 
 void Renderer::set_gl_viewport(const glm::uvec2 & min, const glm::uvec2& max)
 {
+    glViewport(min.x, min.y, max.x, max.y);
 }
 
 void Renderer::clear()
 {
+    glClearColor(0.66f, 0.66f, 0.66f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
 
 void Renderer::swap_buffers()
 {
+    SDL_GL_SwapWindow(m_window);
 }
 
 void Renderer::render()
 {
+    glUseProgram(m_shader_program_id);
+    glBindVertexArray(m_vertex_array_object);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
 void Renderer::on_initialize()
@@ -78,6 +85,10 @@ void Renderer::initialize_open_gl()
     glewInit();
 
     SDL_GL_SetSwapInterval(m_window_data.vertical_synchronization);
+
+    //glEnable(GL_DEPTH_TEST);
+    //glEnable(GL_CULL_FACE);
+    //glCullFace(GL_BACK);
 }
 
 void Renderer::terminate_open_gl()
@@ -87,20 +98,105 @@ void Renderer::terminate_open_gl()
 
 void Renderer::create_shader_vertex()
 {
+    const char* source =
+        "#version 330 core                                       \n"
+        "layout (location = 0) in vec3 pos;                      \n"
+        "void main()                                             \n"
+        "{                                                       \n"
+        "    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);    \n"
+        "}                                                       \0";
+
+    m_shader_vertex_id = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(m_shader_vertex_id, 1, &source, NULL);
+    glCompileShader(m_shader_vertex_id);
+
+    int success;
+    glGetShaderiv(m_shader_vertex_id, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char infoLog[512] = { 0 };
+        glGetShaderInfoLog(m_shader_vertex_id, 512, NULL, infoLog);
+        MANAGER_LOG(std::string("Error: ") + std::string(infoLog));
+    }
 }
 
 void Renderer::create_shader_fragment()
 {
+    const char* source =
+        "#version 330 core                                 \n"
+        "out vec4 FragColor;                               \n"
+        "void main()                                       \n"
+        "{                                                 \n"
+        "    FragColor = vec4(0.5f, 1.0f, 0.33f, 1.0f);    \n"
+        "}                                                 \0";
+
+    m_shader_fragment_id = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(m_shader_fragment_id, 1, &source, NULL);
+    glCompileShader(m_shader_fragment_id);
+
+    int success;
+    glGetShaderiv(m_shader_fragment_id, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char infoLog[512] = { 0 };
+        glGetShaderInfoLog(m_shader_fragment_id, 512, NULL, infoLog);
+        MANAGER_LOG(std::string("Error: ") + std::string(infoLog));
+    }
 }
 
 void Renderer::create_shader_program()
 {
+    m_shader_program_id = glCreateProgram();
+    glAttachShader(m_shader_program_id, m_shader_vertex_id);
+    glAttachShader(m_shader_program_id, m_shader_fragment_id);
+    glLinkProgram(m_shader_program_id);
+
+    int success;
+    glGetProgramiv(m_shader_program_id, GL_LINK_STATUS, &success);
+    if (!success) {
+        char infoLog[512] = { 0 };
+        glGetProgramInfoLog(m_shader_program_id, 512, NULL, infoLog);
+        MANAGER_LOG(std::string("Error: ") + std::string(infoLog));
+    }
 }
 
 void Renderer::create_render_data()
 {
+    const unsigned int vertices_size = 9;
+    const float vertices[vertices_size] = {
+        -0.5f, -0.25f, 0.0f,
+        0.0f, 0.75f, 0.0f,
+        0.5f, -0.25f, 0.0f,
+    };
+
+    const unsigned int indices_size = 3;
+    const unsigned int indices[indices_size] = {
+        0, 1, 2,
+    };
+
+    glGenVertexArrays(1, &m_vertex_array_object);
+    glBindVertexArray(m_vertex_array_object);
+
+    glGenBuffers(1, &m_buffer_object_vertex);
+    glBindBuffer(GL_ARRAY_BUFFER, m_buffer_object_vertex);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices_size, vertices, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &m_buffer_object_indices);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_buffer_object_indices);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices_size, indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
 }
 
 void Renderer::clean_up()
 {
+    glDeleteVertexArrays(1, &m_vertex_array_object);
+    glDeleteBuffers(1, &m_buffer_object_vertex);
+    glDeleteBuffers(1, &m_buffer_object_indices);
+
+    glDeleteShader(m_shader_vertex_id);
+    glDeleteShader(m_shader_vertex_id);
+
+    glDeleteProgram(m_shader_program_id);
 }
